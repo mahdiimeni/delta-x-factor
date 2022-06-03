b3622fa7db792a5bb4cc0283ee8be2e063885ec6
mahdiimeni <mahdi.imeni@gmail.com>
2022-06-03 19:38:46 +0430

    # Fix tabulate header

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from x_factor import x_factor
 from tabulate import tabulate
 
 pages = input("How many pages do you want to check?! > ")
-table = tabulate(x_factor(int(pages)))
 
-print(table)
+tabulate_header = [
+    "market_cap_rank",
+    "id",
+    "symbol",
+    "X Factor",
+]
+
+table = x_factor(int(pages))
+
+tabulate_table = tabulate(
+    table, headers=tabulate_header, showindex="always", tablefmt="psql"
+)
+
+print(tabulate_table)
